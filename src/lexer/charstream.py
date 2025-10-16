d683d112789dd3ee0dbdc0b6c156f1c736e1211f
FityatulhaqRosyidi <fityatulhaqrosyidi25@gmail.com>
2025-10-16 17:32:20 +0700

    feat : add CharStream

--- src/lexer/charstream.py
+++ src/lexer/charstream.py
@@ -1,6 +1,6 @@
 """
 Apa:
-- mesin karakter + tau posisi line/col
+- mesin karakter + tau posisi row/col
 
 Ngapain:
 - load text dari path
@@ -11,3 +11,32 @@ catatan:
 - peek /next return '' aja pas EOF
 """
 
+class CharStream:
+    def __init__(self, text: str):
+        self.text = text
+        self.pos = 0
+        self.row = 1
+        self.col = 1
+
+    def eof(self) -> bool:
+        return self.pos >= len(self.text)
+
+    def peek(self) -> str: 
+        if self.eof():
+            return ''
+        return self.text[self.pos]
+
+    def next(self) -> str:
+        # note : yg dilakukan next adalah (pindah posisi -> peek)
+        self.pos += 1
+        char = self.peek()
+        if char == '\n':
+            self.row += 1
+            self.col = 1
+        else :
+            self.col += 1
+        return char
+
+    def get_pos(self) -> tuple[int, int]:
+        return (self.row, self.col)
+        
