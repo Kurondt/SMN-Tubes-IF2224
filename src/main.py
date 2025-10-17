ecbce8ffdc93094571574cad239181605a1d0dd0
AryoBama <13523088@std.stei.itb.ac.id>
2025-10-17 16:34:16 +0700

    feat: Implement main

--- src/main.py
+++ src/main.py
@@ -2,20 +2,20 @@ from .cli import parse_args
 # from .lexer.rules_loader import DFARules
 # from .lexer.scanner import Scanner
 from .errors import LexError
+from src.lexer.rules_loader import Rule
+from src.lexer.scanner import Scanner
 
 def main():
     args = parse_args()
-    """
-    # contoh kalau implementasi kelar
+
     text = args.source.read_text(encoding="utf-8")
-    rules = DFARules.load(str(args.dfa))
+    rules = Rule().load_rule(args.dfa)
     scanner = Scanner(rules, text)
     try:
-        for tok in scanner.tokens():
+        for tok in scanner.tokenize():
             print(repr(tok))
     except LexError as e:
         print(e)
-    """
 
 if __name__ == "__main__":
     main()
