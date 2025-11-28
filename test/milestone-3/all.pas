d49725b3345371ed5d5178a33314b21cda5a0b5d
Refki Alfarizi <refkialfarizi123@gmail.com>
2025-11-29 00:53:45 +0700

    feat: add semantic analyzer

---
+++ test/milestone-3/all.pas
@@ -0,0 +1,84 @@
+program test_full_tree;
+
+konstanta
+  maxVal = 100;
+  piVal  = 3.14;
+  greet  = 'tbfo';
+
+tipe
+  index = integer;
+  realArray = larik[1..5] dari Real;
+  letter = char;
+  flag = boolean;
+  koordinat = rekaman
+    x: Real;
+    y: Real;
+    valid: boolean;
+  selesai;
+
+variabel
+  x, y, z: integer;
+  sum: Real;
+  avg: Real;
+  count: integer;
+  c: char;
+  s: string;
+  arr: larik[1..5] dari Real;
+  done: boolean;
+
+prosedur ok(msg: char);
+mulai
+  jika tidak done maka
+    writeln('Program ', msg, ' seru sekali')
+  selain_itu
+    writeln('Selesai');
+selesai;
+
+fungsi add(a, b: integer): integer;
+mulai
+  add := a + b;
+selesai;
+
+mulai
+  x := 22;
+  y := 3;
+  z := 2018;
+  sum := x + y * (z bagi 10) - (x mod y);
+  avg := (x + y + z) / 3.0;
+
+  done := false;
+
+  jika (sum >= maxVal) dan tidak done maka
+    done := true
+  selain_itu jika (sum < maxVal) atau (x <> y) maka
+    done := false;
+
+  jika (x <= y) maka
+    done := false;
+
+  selama (x > 0) lakukan
+  mulai
+    x := x - 1;
+  selesai;
+
+  arr[1] := 1.0;
+  arr[2] := 2.5555;  
+  arr[3] := 3.14;
+  arr[4] := 4.0;
+  arr[5] := 2;
+
+  c := 'a';
+  c := 'b';
+  c := 'c';
+  s := 'seru sekali';
+
+  untuk count := 1 ke 5 lakukan
+    sum := sum + count;
+
+  untuk count := 5 turun_ke 1 lakukan
+    avg := avg + arr[count];
+
+  writeln(greet);
+  x := add(10, 20);
+
+selesai.
